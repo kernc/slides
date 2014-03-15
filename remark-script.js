311605b58b38457fddf255f1124e14d3b68ae067
Kernc <kerncece@gmail.com>
2014-03-15 13:40:46 +0100

    Updated template, uses local remark.min.js

--- remark-script.js
+++ remark-script.js
@@ -1,10 +1,13 @@
 var remark_config = {highlightStyle: 'default'};
 var slideshow = remark.create(remark_config);
+
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-39132001-4']);
-_gaq.push(['_trackPageview']);
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+if (window.location.hostname != 'localhost') {
+    _gaq.push(['_setAccount', 'UA-39132001-4']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+}
